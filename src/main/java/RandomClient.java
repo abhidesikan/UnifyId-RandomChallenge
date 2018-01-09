74fc2f72eae614be410a3ef77f9b2b195b6c653c
Abhishek Desikan <abhidesikan@AMBP.attlocal.net>
2018-01-08 20:15:04 -0500

    Generate RSA key/pair using Key pair generator

---
+++ src/main/java/RandomClient.java
@@ -0,0 +1,50 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.URL;
+import java.security.*;
+
+public class RandomClient {
+
+
+	private static String randomUrl = "https://www.random.org/integers/?";
+
+	public static String getRandomNumber(Integer num, Integer min, Integer max, Integer col, int base, String format, String rnd) throws Exception {
+
+		randomUrl = randomUrl + "num=" + num + "&min=" + min + "&max=" + max + "&col=" + col + "&base=" + base
+				+ "&format=" + format + "&rnd=" + rnd + "/";
+		URL myUrl = new URL(randomUrl);
+		HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
+		InputStream is = conn.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String inputLine;
+		String value = "";
+
+		while ((inputLine = br.readLine()) != null) {
+			value = inputLine;
+		}
+
+		br.close();
+		return value;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+
+		SecureRandom random = new SecureRandom();
+		byte[] bytes = random.generateSeed(Integer.parseInt(getRandomNumber(1, 1, 10, 1, 10, "plain", "new")));
+
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(1024, random);
+		KeyPair pair = keyPairGenerator.generateKeyPair();
+		PrivateKey priv = pair.getPrivate();
+		PublicKey pub = pair.getPublic();
+
+		System.out.println(priv);
+		System.out.println(pub);
+	}
+}
